98d535f8c9f5e7dbbe5a916b6d66fbef44fd2c4f
성찬 <ysc1086@gmail.com>
2025-07-27 21:03:26 +0900

    Initial commit

---
+++ backend/src/main/java/com/example/demo/JwtBoardSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtBoardSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtBoardSpringbootApplication.class, args);
+	}
+
+}
