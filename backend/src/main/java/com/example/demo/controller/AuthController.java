8450d6e2cc1561273fc23c32e047ecd0e99fdde0
성찬 <ysc1086@gmail.com>
2025-07-28 22:57:28 +0900

    로그인 엑세스 토큰

--- backend/src/main/java/com/example/demo/controller/AuthController.java
+++ backend/src/main/java/com/example/demo/controller/AuthController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.dto.LoginRequest;
 import com.example.demo.dto.RegisterRequest;
 import com.example.demo.service.AuthService;
 
@@ -23,4 +24,11 @@ public class AuthController {
         authService.register(request);
         return ResponseEntity.ok("회원가입 성공");
     }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> postMethodName(@RequestBody LoginRequest request) {
+        String token = authService.login(request);
+        return ResponseEntity.ok(token);
+    }
+    
 }
