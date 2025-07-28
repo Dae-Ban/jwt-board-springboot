6d5040522a1d8fd7bc0b0b809cd92afc7f693811
성찬 <ysc1086@gmail.com>
2025-07-28 22:03:23 +0900

    401, 403 처리

--- backend/src/main/java/com/example/demo/config/SecurityConfig.java
+++ backend/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -13,7 +13,10 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 import com.example.demo.jwt.JwtAuthenticationFilter;
 import com.example.demo.jwt.JwtTokenProvider;
+import com.example.demo.response.ApiResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 
 @Configuration
@@ -21,6 +24,7 @@ import lombok.RequiredArgsConstructor;
 public class SecurityConfig {
 
     private final JwtTokenProvider jwtTokenProvider;
+    private final ObjectMapper objectMapper;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -31,10 +35,29 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll() // 로그인, 회원가입은 허용
-                        .anyRequest().authenticated()
-                )
+                        .anyRequest().authenticated())
                 .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),
                         UsernamePasswordAuthenticationFilter.class)
+
+                .exceptionHandling(exception -> exception
+                        // 401
+                        .authenticationEntryPoint((request, response, authException) -> {
+                            ApiResponse<?> errorResponse = ApiResponse.failure(request,
+                                    HttpServletResponse.SC_UNAUTHORIZED, "인증이 필요합니다.");
+                            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                            response.setContentType("application/json;charset=UTF-8");
+                            response.getWriter().write(objectMapper.writeValueAsString(errorResponse));
+                        })
+
+                        // 403
+                        .accessDeniedHandler((request, response, accessDeniedException) -> {
+                            ApiResponse<?> errorResponse = ApiResponse.failure(request,
+                                    HttpServletResponse.SC_FORBIDDEN, "접근 권한이 없습니다.");
+                            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+                            response.setContentType("application/json;charset=UTF-8");
+                            response.getWriter().write(objectMapper.writeValueAsString(errorResponse));
+                        }))
+
                 .build();
     }
 
